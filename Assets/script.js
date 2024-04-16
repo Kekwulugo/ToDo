577d43a8c5bbee0c64507f36236c157480ab0d21
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-15 19:56:36 -0600

    Add task class

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,10 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const openModalBtn = document.querySelector(".btn-open");
 const closeModalBtn = document.querySelector(".btn-close");
+const taskSubmit = document.querySelector(".btn-submit");
+
+//create project Library 
+let myTasks = [];
 
 openModalBtn.addEventListener('click', function(){
  modal.classList.remove('hidden');
@@ -28,13 +32,22 @@ class Tasks{
  }
 }
 
+taskSubmit.addEventListener('click', function(event){
+ event.preventDefault();
 
+ //get input details
+ let taskTitle = document.querySelector("#title").value;
+ let taskDueDate = document.querySelector("#due-date").value;
+ let taskPriority = document.querySelector("#priority").options.selectedIndex;
+ let taskNotes = document.querySelector("#notes").value;
+ //let taskProject = document.querySelector("#project").options.selectedIndex;
 
-let task1 = new Tasks("Chores","Today","Medium","Do not forget to do this!","Home");
+let newTask = new Tasks(taskTitle,taskDueDate,"",taskNotes,"");
+console.log(newTask);
 
-let Projects = [];
-Projects.push(task1);
+myTasks.push(newTask);
+ 
 
-console.log(task1);
-console.log(Projects);
+});
+
 
