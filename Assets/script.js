5e2b4c668d99e66e20911099eadc705eb7ae5186
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-17 14:33:30 -0600

    Add project form & rendering functions

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,9 @@
-//Control Form Modal
+// Control Modals
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
-const closeModalBtn = document.querySelector(".btn-close");
-const taskSubmit = document.querySelector(".btn-submit");
+
 
 //create project Library 
 let myTasks = [];
-
-openModalBtn.addEventListener('click', function(){
- modal.classList.remove('hidden');
- overlay.classList.remove('hidden');
-
-});
-
-closeModalBtn.addEventListener('click',function(){
- modal.classList.add('hidden');
- overlay.classList.add('hidden');
-})
-
 
 class Tasks{
  constructor(title,dueDate, priority,notes, project){
@@ -30,7 +14,9 @@ class Tasks{
   this.project = project;
 
  }
-}
+};
+
+let taskSubmit = document.querySelector(".task-submit");
 
 taskSubmit.addEventListener('click', function(event){
  event.preventDefault();
@@ -47,11 +33,99 @@ taskSubmit.addEventListener('click', function(event){
 
 let newTask = new Tasks(taskTitle,taskDueDate,taskPriority,taskNotes,taskProject);
 
-console.log(newTask);
-
 myTasks.push(newTask);
- 
+console.log(myTasks);
 
+renderTasks();
+
+taskModal.classList.add('hidden');
+overlay.classList.add('hidden');
+ 
 });
+
+
+
+function renderTasks(){
+ let taskContainer = document.querySelector(".task-container");
+
+ taskContainer.innerHTML = "";
+
+ for (let i = 0; i <myTasks.length; i++){
+
+  const taskItem = document.createElement("div");
+  taskItem.classList.add("task-item");
+
+ const title = document.createElement("p");
+ title.innerText = myTasks[i].title;
+
+ const dueDate = document.createElement("p");
+ dueDate.innerText = myTasks[i].dueDate;
+
+
+ const priority =document.createElement("p");
+ priority.innerText = myTasks[i].priority;
+
+ const notes = document.createElement("p");
+ notes.innerText = myTasks[i].notes;
+
+ const project = document.createElement("p");
+ project.innerText = myTasks[i].project;
+
+ taskItem.append(title);
+ taskItem.append(dueDate);
+ taskItem.append(priority);
+ taskItem.append(notes);
+ taskItem.append(project);
+
+ taskContainer.append(taskItem);
+
+ }
+};
+
+function renderProjects(){
+ let projectContainer = document.querySelector(".project-container");
+ projectContainer.innerHTML ="";
+
+ for (let i = 0; i<myProjects.length;i++){
+
+ let projectItem = document.createElement('div');
+ projectItem.classList.add("project-item");
+ let itemTitle = document.createElement('p');
+ itemTitle.innerText = myProjects[i].projectTitle;
+
+ projectItem.append(itemTitle);
+ projectContainer.append(projectItem);
+}
+}
+
+let myProjects = [];
+
+class Projects{
+ constructor (index, projectTitle, tasks){
+  this.id = index;
+  this.projectTitle = projectTitle;
+  this.tasks = tasks;
+ }
+};
+
+let projectSumbit = document.querySelector(".project-submit")
+let currentId = 0;
+
+projectSumbit.addEventListener('click', function(event){
+ event.preventDefault();
+
+ let Title = document.querySelector("#project-title").value;
+
+ currentId++;
+
+ let newProject = new Projects(currentId,Title,myTasks);
+ myProjects.push(newProject);
+
+ renderProjects();
+
+ console.log(myProjects);
+ 
+});
+
 
 
